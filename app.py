ea921969d7fbd6cb513b5803fcb5cedcf1ee6d71
Sowmyakakarlamudi <133688791+Sowmyakakarlamudi@users.noreply.github.com>
2026-02-09 12:15:03 +0530

    Implement CSRF protection and modify greeting
    Added CSRF protection and updated response message.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+import os
+
 app = Flask(__name__)
+
+# Secret key for security
+app.config["SECRET_KEY"] = os.urandom(32)
+
+# Enable CSRF protection
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def hello():
-    return "Hello from GitHub Actions Docker Pipeline!"
+    return "Hello from Docker CI/CD Pipeline with Security!"
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
